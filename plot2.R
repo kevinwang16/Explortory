cc8cd12226d6d9d5e8bf0e79112c405a33602560
kevinwang16 <kevinwang816@gmail.com>
2015-04-12 19:20:07 +0300

    plot2.R

---
+++ plot2.R
@@ -0,0 +1,26 @@
+#set direction
+getwd()
+setwd("C:/Users/Xi/Documents/Explortory data analysis")
+
+#get data
+data1 <- read.table("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"') 
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+dim(data1)
+attach(data1)
+head(data1)
+subset <- Date == "2007-02-01" | Date == "2007-2-2"
+data2<-data1[subset,]
+head(data2)
+
+datetime <- paste(as.Date(data2$Date), data2$Time) 
+data2$Datetime <- as.POSIXct(datetime) 
+
+
+
+#plot
+plot(data2$Global_active_power~data2$Datetime, type="l", 
+  ylab="Global Active Power (kilowatts)", xlab="") 
+dev.copy(png, file="plot2.png", height=4800, width=4800) 
+dev.off() 
+
+
